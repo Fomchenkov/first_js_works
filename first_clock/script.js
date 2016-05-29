23f786df1a688c7059092526d4c300c7cb33f680
Fomchenkov Vyacheslav <slava290900@outlook.com>
2016-05-29 11:30:34 +0300

    Add files via upload

---
+++ first_clock/script.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+	
+	timenow(); //вызов часов сразу после загрузки
+
+	document.body.children[0].className = 'time';
+
+	setInterval(function(){timenow()}, 1000);
+
+	function timenow() {
+
+		var now = 	new Date();
+		var Hours = now.getHours();
+		var Minutes = now.getMinutes();
+		var Seconds = now.getSeconds();
+
+		if (Hours < 10) { "0" + Hours};
+		if (Minutes < 10) { "0" + Minutes};
+		if (Seconds < 10) { "0" + Seconds};
+
+		document.body.children[0].innerHTML = 
+		(Hours + ':'+ Minutes + ':' + Seconds);
+	};
+};
